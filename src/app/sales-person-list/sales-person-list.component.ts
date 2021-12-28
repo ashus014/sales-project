c31d057875b0706a2c7cdf974466e9463bacb079
Ashu Singh <ashu.ask.singh@gmail.com>
2021-12-28 13:36:32 +0530

    Create a class and print their objects on screen

---
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  //create an array of objects
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Chirstian", "Bale", "bale@123.com", 50000),
+    new SalesPerson("John", "Rogers", "rogers@123.com", 60000),
+    new SalesPerson("Sam", "Mendez", "medez@123.com", 70000),
+    new SalesPerson("Chris", "Evans", "even@123.com", 80000)
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
